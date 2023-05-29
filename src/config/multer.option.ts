672eab1cb39e1abf939f08b48b929ebcf198cfdc
lsh <34882947+lsh940131@users.noreply.github.com>
2023-05-29 17:29:17 +0900

    FIX] multer option => file format error handling

--- src/config/multer.option.ts
+++ src/config/multer.option.ts
@@ -1,27 +1,28 @@
-import { existsSync, mkdirSync } from 'fs';
-import { diskStorage } from 'multer';
+import { UnsupportedMediaTypeException } from "@nestjs/common";
+import { existsSync, mkdirSync } from "fs";
+import { diskStorage } from "multer";
 
 export const multerOptions = {
-  fileFilter: (request, file, callback) => {
-    if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Invalid image format.'));
-    }
-  },
+	fileFilter: (request, file, callback) => {
+		if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+			callback(null, true);
+		} else {
+			callback(new UnsupportedMediaTypeException("Invalid file format."), false);
+		}
+	},
 
-  storage: diskStorage({
-    destination: (request, file, callback) => {
-      const uploadPath: string = 'uploads';
+	storage: diskStorage({
+		destination: (request, file, callback) => {
+			const uploadPath: string = "uploads";
 
-      if (!existsSync(uploadPath)) {
-        mkdirSync(uploadPath);
-      }
+			if (!existsSync(uploadPath)) {
+				mkdirSync(uploadPath);
+			}
 
-      callback(null, uploadPath);
-    },
-    filename: (request, file, callback) => {
-      callback(null, file.originalname);
-    },
-  }),
+			callback(null, uploadPath);
+		},
+		filename: (request, file, callback) => {
+			callback(null, file.originalname);
+		},
+	}),
 };
--- src/gallery/gallery.controller.ts
+++ src/gallery/gallery.controller.ts
@@ -1,45 +1,38 @@
-import {
-  Controller,
-  Post,
-  UseInterceptors,
-  UploadedFile,
-  UploadedFiles,
-} from '@nestjs/common';
-import {
-  FileInterceptor,
-  FileFieldsInterceptor,
-} from '@nestjs/platform-express';
-import { multerOptions } from '../config/multer.option';
+import { Controller, Post, UseInterceptors, UploadedFile, UploadedFiles } from "@nestjs/common";
+import { FileInterceptor, FileFieldsInterceptor } from "@nestjs/platform-express";
+import { multerOptions } from "../config/multer.option";
 
-@Controller('gallery')
+@Controller("gallery")
 export class GalleryController {
-  constructor() {}
+	constructor() {}
 
-  @Post('/upload/single')
-  @UseInterceptors(FileInterceptor('file', multerOptions))
-  async upload(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-  }
+	@Post("/upload/single")
+	@UseInterceptors(FileInterceptor("file", multerOptions))
+	async upload(@UploadedFile() file: Express.Multer.File) {
+		console.log(file);
 
-  @Post('/upload/multi')
-  @UseInterceptors(
-    FileFieldsInterceptor(
-      [
-        { name: 'avatar', maxCount: 2 },
-        { name: 'background', maxCount: 2 },
-      ],
-      multerOptions,
-    ),
-  )
-  uploadFile(
-    @UploadedFiles()
-    files: {
-      avatar?: Express.Multer.File[];
-      background?: Express.Multer.File[];
-    },
-  ) {
-    console.log(files);
-  }
+		return true;
+	}
+
+	@Post("/upload/multi")
+	@UseInterceptors(
+		FileFieldsInterceptor(
+			[
+				{ name: "avatar", maxCount: 2 },
+				{ name: "background", maxCount: 2 },
+			],
+			multerOptions,
+		),
+	)
+	uploadFile(
+		@UploadedFiles()
+		files: {
+			avatar?: Express.Multer.File[];
+			background?: Express.Multer.File[];
+		},
+	) {
+		console.log(files);
+	}
 }
 
 // multer 써서 파일 업로드해가지고 속성 까서 업로드 이미지의 찍힌 시간 순으로 정렬 가능한지 가늠하기
