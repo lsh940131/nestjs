90e6ef09cc44851b46eca082c8eba8150389e09c
lsh <34882947+lsh940131@users.noreply.github.com>
2023-06-21 16:24:08 +0900

    ENHANCE] swagger docs 접근 보안

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./v1/app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as expressBasicAuth from "express-basic-auth";
+import { SWAGGER_USERS } from "../env.js";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(
+		["/docs"],
+		expressBasicAuth({
+			challenge: true,
+			users: SWAGGER_USERS,
+		}),
+	);
 
 	const config = new DocumentBuilder().setTitle("Swagger 타이틀").setDescription("Swagger description").setVersion("서버 버전").build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("api-docs", app, document);
+	SwaggerModule.setup("docs", app, document);
 
 	await app.listen(3000);
 }
