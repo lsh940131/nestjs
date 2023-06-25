7f3164e3fa393c83b24ecd0e0f9f69f3fa6d474c
lsh <l940131@gmail.com>
2023-06-25 21:39:22 +0900

    TEMP] nest-mysql2 testing...

--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -14,8 +14,29 @@ export class UserController {
 	@ApiCreatedResponse({ description: "유저 생성", type: Object })
 	@UseInterceptors()
 	async createUser(@Body() body: UserCreateDto): Promise<Object> {
-		const id = await this.userService.create(body);
+		// const id = await this.userService.create(body);
 
+		const { email, name } = body;
+		const conn1 = await this.db.getConnection();
+		await this.db.transaction(conn1);
+		const [r1] = await conn1.query({ conn: conn1, sql: "insert into user set email='abcde', name='abc'", value: { email, name } });
+		console.log(r1.insertId);
+
+		console.time("process");
+		await new Promise((resolve, reject) => {
+			setTimeout(async () => {
+				console.timeLog("process");
+				await conn1.commit();
+				conn1.release();
+				resolve(0);
+			}, 5000);
+		});
+
+		console.timeEnd("process");
+
+		console.log(">> commit");
+
+		const id = r1.insertId;
 		return { id };
 	}
 
@@ -43,6 +64,16 @@ export class UserController {
 	async getUserQuery(@Query() query: UserReadDto): Promise<Object> {
 		// const result = await this.mysql.query("select * from user");
 		// console.log(result);
+
+		const conn1 = await this.db.getConnection();
+		const conn2 = await this.db.getConnection();
+
+		const [r1] = await conn1.query({ conn: conn1, sql: "SELECT CONNECTION_ID()" });
+		const [r2] = await conn2.query({ conn: conn2, sql: "SELECT CONNECTION_ID()" });
+		console.log(r1, r2);
+
+		conn1.release();
+		conn2.release();
 
 		return 1;
 	}
