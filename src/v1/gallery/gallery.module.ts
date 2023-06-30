5b4c44ebce198cab6a0c5906bc5683cd70b98335
lsh <34882947+lsh940131@users.noreply.github.com>
2023-06-30 14:00:45 +0900

    ENHANCE] user => nestia의 Route, Body, Query 적용 테스트

--- src/v1/gallery/gallery.module.ts
+++ src/v1/gallery/gallery.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { GalleryController } from './gallery.controller';
-import { GalleryService } from './gallery.service';
+import { Module } from "@nestjs/common";
+import { GalleryController } from "./gallery.controller";
+import { GalleryService } from "./gallery.service";
 
-@Module({controllers: [GalleryController, ], providers: [GalleryService]})
+@Module({ controllers: [GalleryController], providers: [GalleryService] })
 export class GalleryModule {}
--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -4,16 +4,19 @@ import { ApiOperation, ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
 import { UserCreateDto, UserReadDto } from "../../dto";
 import { ConfigService } from "@nestjs/config";
 import { MysqlService } from "../../lib/db/mysql/mysql.service";
+
+import { TypedRoute, TypedQuery, TypedBody } from "@nestia/core";
+
 @Controller("user")
 @ApiTags("user")
 export class UserController {
 	constructor(private readonly userService: UserService, private readonly config: ConfigService, private readonly db: MysqlService) {}
 
-	@Post("/")
+	@TypedRoute.Post("/")
 	@ApiOperation({ summary: "유저 생성", description: "유저 생성" })
 	@ApiCreatedResponse({ description: "유저 생성", type: Object })
 	@UseInterceptors()
-	async createUser(@Body() body: UserCreateDto): Promise<Object> {
+	async createUser(@TypedBody() body: UserCreateDto): Promise<{ id: number }> {
 		// const id = await this.userService.create(body);
 
 		const { email, name } = body;
@@ -22,38 +25,37 @@ export class UserController {
 		const r1 = await conn1.query("insert into user set ?", [{ email, name }]);
 		console.log(r1.insertId);
 
-		console.time("process");
-		await new Promise((resolve, reject) => {
-			setTimeout(async () => {
-				console.timeLog("process");
-				await conn1.commit();
-				conn1.release();
-				resolve(0);
-			}, 5000);
-		});
-
-		console.timeEnd("process");
-
-		console.log(">> commit");
+		// console.time("process");
+		// await new Promise((resolve, reject) => {
+		// 	setTimeout(async () => {
+		// 		console.timeLog("process");
+		// 		await conn1.commit();
+		// 		conn1.release();
+		// 		resolve(0);
+		// 	}, 5000);
+		// });
+		// console.timeEnd("process");
+		// console.log(">> commit");
+		await conn1.commit();
+		conn1.release();
 
 		const id = r1.insertId;
 		return { id };
 	}
 
-	@Get("/")
+	@TypedRoute.Get("/")
 	@ApiOperation({ summary: "유저 조회", description: "유저 조회" })
 	@ApiCreatedResponse({ description: "유저 조회", type: UserReadDto })
-	async getUser(@Query() query: UserReadDto): Promise<Object> {
+	async getUser(@TypedQuery() query: UserReadDto): Promise<any> {
 		const r = await this.db.query("select * from user");
-		console.log(r);
 
-		return 1;
+		return r;
 	}
 
-	@Get("/query")
+	@TypedRoute.Get("/query")
 	@ApiOperation({ summary: "유저 조회", description: "유저 조회" })
 	@ApiCreatedResponse({ description: "유저 조회", type: Object })
-	async getUserQuery(@Query() query: UserReadDto): Promise<Object> {
+	async getUserQuery(@TypedQuery() query: UserReadDto): Promise<number> {
 		const [c1, c2] = await Promise.all([this.db.getTransaction(), this.db.getTransaction()]);
 
 		const r1 = await c1.query("SELECT CONNECTION_ID();");
