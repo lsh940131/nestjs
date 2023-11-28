cdc8e07673483ad30e6902c4a8cc5488ac3ec80f
lsh <34882947+lsh940131@users.noreply.github.com>
2023-11-28 13:33:03 +0900

    REMOVE] ALL

--- src/lib/db/mysql/interfaces/transaction.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ITransaction {
-	beginTransaction: () => Promise<void>;
-	query: (sql: string, value?: any) => Promise<any>;
-	commit: () => Promise<void>;
-	rollback: () => Promise<void>;
-	release: () => void;
-}
--- src/lib/db/mysql/mysql.util.ts
+++
@@ -1,3 +0,0 @@
-import { MYSQL_DEFAULT_TOKEN_NAME } from "./mysql.constants";
-
-export const getToken = (tokenName: string = MYSQL_DEFAULT_TOKEN_NAME) => tokenName;
