7bf40e3e082aac27cdc9129fb2077221f75d20c2
lsh <34882947+lsh940131@users.noreply.github.com>
2023-11-30 17:01:05 +0900

    ADD] swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import * as expressBasicAuth from "express-basic-auth";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.use(["/api/docs"], expressBasicAuth({ challenge: true, users: { ["lsh"]: "940131" } }));
+
+	const config = new DocumentBuilder().setTitle("API Document").setDescription("description").setVersion("0.1").build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("/api/docs", app, document);
+
+	await app.listen(3000);
 }
 bootstrap();
