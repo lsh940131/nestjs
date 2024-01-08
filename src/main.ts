9de2a60173f839b3a43a0e30c56f394c212c1e8e
lsh <34882947+lsh940131@users.noreply.github.com>
2024-01-08 14:44:59 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 import { setupSwagger } from "./swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	// validate pipe
+	// validate
 	app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true }, whitelist: true }));
 
 	// swagger
