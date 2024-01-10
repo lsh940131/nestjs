1829fb5dcf458cb3e4fdc97e4d6dcc8f85e67d83
lsh <34882947+lsh940131@users.noreply.github.com>
2024-01-10 17:49:55 +0900

    ENHANCE] prisma => query를 어떻게 짜나 확인하기 위함

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,17 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, "query" | "error"> implements OnModuleInit {
+	constructor() {
+		super({
+			log: ["query"],
+		});
+	}
+
 	async onModuleInit() {
+		this.$on("query", (e) => console.log(e));
+
 		await this.$connect();
 	}
 }
